c32629ef2600d5e32a100bb2320e1b873e866f64
Joakim Ekblad <aio.github@aio.nu>
2017-06-25 21:11:50 +0200

    Add CUTEST_COVERAGE_DIR varibale to set the path from where to start searching for coverage files

--- src/coverage.mk
+++ src/coverage.mk
@@ -19,16 +19,17 @@
 #
 
 CUTEST_CFLAGS+=-fprofile-arcs -ftest-coverage
+CUTEST_COVERAGE_DIR?=.
 
 coverage.xml: check
-	$(Q)gcovr -r . -e '/usr.*' -e '.*_test.c' -e 'cutest.h' -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' -x > $@
+	$(Q)gcovr -r $(CUTEST_COVERAGE_DIR) -e '/usr.*' -e '.*_test.c' -e 'cutest.h' -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' -x > $@
 
 check::
 	$(Q)echo "Lines covered:"; \
-	gcovr -r . -e '/usr.*' -e '.*_test.c' -e '.*cutest.h' -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' | egrep -v '^File' | egrep -v '^-' | egrep -v '^Directory' | grep -v 'GCC Code'; \
+	gcovr -r $(CUTEST_COVERAGE_DIR) -e '/usr.*' -e '.*_test.c' -e '.*cutest.h' -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' | egrep -v '^File' | egrep -v '^-' | egrep -v '^Directory' | grep -v 'GCC Code'; \
 	echo ""; \
 	echo "Branches covered:"; \
-	gcovr -r . -e '/usr.*' -e '.*_test.c' -e '.*cutest.h'  -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' -b | egrep -v '^File' | egrep -v '^-' | egrep -v '^Directory' | grep -v 'GCC Code';
+	gcovr -r $(CUTEST_COVERAGE_DIR) -e '/usr.*' -e '.*_test.c' -e '.*cutest.h'  -e '.*_mocks.h' -e 'error.h' -e '.*_test_run.c' -b | egrep -v '^File' | egrep -v '^-' | egrep -v '^Directory' | grep -v 'GCC Code';
 
 clean::
 	$(Q)rm -rf coverage.xml *.gcno *.gcda
